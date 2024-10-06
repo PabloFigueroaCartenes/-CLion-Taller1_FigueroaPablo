21b7c7cc9201884ab24bcd2680b1d729816000df
Pablo Figueroa <pablo.figueroa@alumnos.ucn.cl>
2024-10-06 16:18:27 -0300

    Avance

---
+++ cmake-build-debug/CMakeFiles/control1_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control1_1.dir/main.cpp.o"
+  "CMakeFiles/control1_1.dir/main.cpp.o.d"
+  "control1_1"
+  "control1_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/control1_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for control1_1.
+# This may be replaced when dependencies are built.
